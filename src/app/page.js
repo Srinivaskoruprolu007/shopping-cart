23cea2595fa00fb6e5df1fa96ff1959588612ec8
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-22 19:13:01 +0530

    basic setup

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import products from "@/data";
+import ProductCard from "@/components/ProductCard";
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
